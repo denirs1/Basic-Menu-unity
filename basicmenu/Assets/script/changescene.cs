399fa22aae23bc312a9bb177a3da75f616b65c51
denirs1 <denis2215@gmail.com>
2018-05-25 19:40:25 +0200

    Update-resolution-and-sound

---
+++ basicmenu/Assets/script/changescene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changescene : MonoBehaviour {
+    
+	//change scene
+	public void change (string scena){
+        Application.LoadLevel(scena);
+	}
+	
+}
